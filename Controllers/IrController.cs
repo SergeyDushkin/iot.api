67234f9a394e3fa761515c843aa6f4590dd4b055
Sergey Dushkin <leirby@live.ru>
2017-04-25 22:47:41 +0300

    Add project files.

---
+++ Controllers/IrController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using iot.api.Domain;
+using Nest;
+
+namespace iot.api.Controllers
+{
+    [Route("api/ir/{manufacturer}")]
+    public class IrController : Controller
+    {
+        private readonly IElasticClient client;
+
+        public IrController(IElasticClient client)
+        {
+            this.client = client;
+        }
+
+        [HttpGet]
+        public IEnumerable<IRDeviceSetting> Get(string manufacturer)
+        {
+            return client.Search<IRDeviceSetting>(s => s.Index("ir"))
+                .Documents
+                .Where(r => r.Manufacturer == manufacturer);
+        }
+        
+        [HttpGet("{model}")]
+        public IRDeviceSetting Get(string manufacturer = null, string model = null)
+        {
+            return client.Search<IRDeviceSetting>(s => s.Index("ir"))
+                .Documents
+                .Where(r => r.Manufacturer == manufacturer && r.Model == model)
+                .FirstOrDefault();
+        }
+        
+        [HttpPost]
+        public void Post([FromBody]IRDeviceSetting value)
+        {
+            var id = value.Manufacturer + ":" + value.Model;
+
+            var response = client.Update<IRDeviceSetting, object>(id, u => u.Index("ir").Doc(value).DocAsUpsert());
+        }
+        
+        [HttpDelete("{model}")]
+        public void Delete(string manufacturer, string model)
+        {
+            var id = manufacturer + ":" + model;
+            var response = client.Delete<IRDeviceSetting>(id, d => d.Index("ir"));
+        }
+    }
+}
---
+++ Domain/IRDeviceSetting.cs
@@ -0,0 +1,43 @@
+﻿namespace iot.api.Domain
+{
+    public class IRDeviceSetting
+    {
+        public string Manufacturer { get; set; }
+        public string Model { get; set; }
+        public string Template { get; set; }
+        public int Command_length { get; set; }
+        public OneStruct One { get; set; }
+        public OneStruct Zero { get; set; }
+        public int Post_data_bits { get; set; }
+        public int Repeat_gap { get; set; }
+        public int Min_repeat { get; set; }
+        public PropertyStruct Properties { get; set; }
+
+        public class HeaderStruct
+        {
+            public int Pulse { get; set; }
+            public int Space { get; set; }
+        }
+
+        public class OneStruct
+        {
+            public int Pulse { get; set; }
+            public int Space { get; set; }
+        }
+
+        public class ZeroStruct
+        {
+            public int Pulse { get; set; }
+            public int Space { get; set; }
+        }
+
+        public class PropertyStruct
+        {
+            public HeaderStruct Header { get; set; }
+            public string Command { get; set; }
+            public int Post_data { get; set; }
+            public int Ptrail { get; set; }
+            public int Gap { get; set; }
+        }
+    }
+}
